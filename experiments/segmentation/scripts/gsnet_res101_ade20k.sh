480eb6aca087e6bde6299dfc54b7a06ed6e61c85
yougoforward <908865817@qq.com>
2020-06-20 17:36:51 +0800

    fcn

--- experiments/segmentation/scripts/gsnet_res101_ade20k.sh
+++ experiments/segmentation/scripts/gsnet_res101_ade20k.sh
@@ -5,11 +5,11 @@ python -m experiments.segmentation.train --dataset ade20k \
     --backbone resnet101 --checkname gsnet_res101_ade20k
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset ade20k \
+python -m experiments.segmentation.test_whole --dataset ade20k \
     --model gsnet --aux --dilated --base-size 576 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/ade20k/gsnet/gsnet_res101_ade20k/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset ade20k \
+python -m experiments.segmentation.test_whole --dataset ade20k \
     --model gsnet --aux --dilated --base-size 576 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/ade20k/gsnet/gsnet_res101_ade20k/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/gsnet_res101_cocostuff.sh
+++ experiments/segmentation/scripts/gsnet_res101_cocostuff.sh
@@ -5,11 +5,11 @@ python -m experiments.segmentation.train --dataset cocostuff \
     --backbone resnet101 --checkname gsnet_res101_cocostuff
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset cocostuff \
+python -m experiments.segmentation.test_whole --dataset cocostuff \
     --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/cocostuff/gsnet/gsnet_res101_cocostuff/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset cocostuff \
+python -m experiments.segmentation.test_whole --dataset cocostuff \
     --model gsnet --aux --dilated --base-size 576 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/cocostuff/gsnet/gsnet_res101_cocostuff/model_best.pth.tar --split val --mode testval --ms
